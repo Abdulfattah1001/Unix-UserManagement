c58c93015582982527acc746e3822cd8a47ba2f8
Abdulfattah <aminufattah6@gmail.com>
2024-07-03 18:07:49 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,35 @@
+#A Unix User Management With Bash Script
+
+##Introduction
+
+Unix-likeoperating systems provide powerful tools for user and group management through command-line (CLI) interfaces. Automation of user management tasks using bash scripting enhances efficiency and ensures consistency.
+This report explores a detailed bash script designed for managing users, groups, home directories, passwords,and secure logging on Unix systems.
+
+#Script Overview
+The bash is structured to read from an inmput formatted as `user;groups`. It performs several key tasks for each user specified in the input file supply as argument to the bash script:
+
+1: Create a user if it does not exist using `useradd`
+2: Create a personal group for the user as specified in the input file using `groupad`
+3: Parse additional groups from the input file separated by `,`
+4: Create any additional groups that do not exist using `groupadd`
+5: Adds the user to all specified groups using `usermod`
+6: Generate a random password using `pwgen`
+7: Sets generated password for each user using `chpasswd`
+8: Stores the username and password securely in /var/secure/user_passwords.txt
+9:Create a home directory for each user under /home.
+10: Sets ownership using `chown` and permissions using `chmod` for each home directory to ensure security and privacy
+11: Logs all performs action into /var/log/user_management.log
+
+##Script implementation
+
+For each user and specified in  the input file
+
+*: Check if the user exits already using `id`
+*: Create the user if it doesn't 
+*: Creates a personal group for the user as specified in the input line of the files
+*: Adds the user to their perosnla groups and any additional groups separated by `,`
+*: Generate Password (14-character) randomlly using `pwgen -s 14 1`
+*: Sets thegenerated password for each user using `chpasswd`
+*: Creates a homedirectory for each user under /home using `mkdir`
+*: Sets ownership of the directoyr using `chown`
+*: Sets permission using   `chmod 700`
---
+++ create_users.sh
@@ -0,0 +1,90 @@
+#! /bin/bash
+
+#Test if the scrip is run with a file
+if [ $# -ne 1 ]; then
+	echo "Error Occurred: Usage : $0 <input_file>"
+	#if not exit the script
+	exit 1
+fi
+
+#global variable to hold the path to the log file
+LOG_FILE="/var/log/user_management.log"
+
+#function to log messages
+log_message(){
+	local log_content="$1"
+	sudo echo "$(date '+%Y-%m-%d %H:%M:$S') - ${log_content}" >> "$LOG_FILE"
+}
+
+
+#A function to generate a random password for a user
+generate_pssd(){
+	pwgen -s 14 1
+}
+
+input_file=$1
+
+#Main code starts here .CODE
+while IFS=';' read -r user group;
+		do
+			#Check wethear the user and the group was specified in the file line
+			if [ -n "$user" ] && [ -n "$group" ]
+				then echo "Processing -> $user $group"
+
+				#Check if the group is existing alredy, if not create it
+				if ! getent group "$group" >/dev/null
+					then log_message "Creating Group $group"
+						sudo groupadd "$group"
+						log_message "Group created succesfully $group"
+				else
+					log_message "Group already exist"
+				fi
+
+				#Check if the user is not existing before
+				if ! id "$user" >/dev/null 2>&1
+					then
+						log_message "Creating user $user"
+						#add the user to the specified group
+						sudo useradd -a -G "$group" "$user"
+						log_message "user group created and added successfully $user"
+						#handling additional groups for the user if the lines contains multiple groups (a,b,c)
+						IFS=',' read -ra
+						additional_groups <<< "$group"
+						for group in "${additional_groups[@]}"
+							do
+								if ! getent group "$group" >/dev/null
+									then sudo groupadd "$group"
+									log_message "Created group successfully"
+								fi
+									sudo usermod -a -G"$group" "$user"
+								done
+
+						#set password for the user
+						password=$(generate_pssd)
+						echo "$user:$password" | sudo chpasswd
+						log_message "set password for user:$user"
+
+						echo "$user:$password" >> /var/secure/user_passwords.txt
+
+						log_message "Password stored for user $user successfully"
+
+						#Creating the home directory for the user in question
+						home_dir="/home/$user"
+						sudo mkdir -p "$home_dir"
+						sudo chown "$user:$user" "$home_dir"
+						#Changing the user permission
+						sudo chmod 700 "$home_dir"
+						log_message "Home directory created successfully for user $user"
+				else
+					log_message "User already exit"
+				fi
+			else
+				#
+				log_message "Skipping line: '$user;$group'.Invalid format"
+				continue
+
+			fi
+done < "$input_file"
+
+#End of code
+log_message "User and group management completed Succesfully"
